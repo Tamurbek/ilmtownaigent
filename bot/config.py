ce7e148d076fe1405be9802413c2aebf82299e37
Diyor <84014882+xobiytv@users.noreply.github.com>
2026-04-29 09:44:46 +0500

    yangilandi

--- bot/config.py
+++ bot/config.py
@@ -13,12 +13,12 @@ class Config:
     """Bot uchun barcha sozlamalar"""
 
     # Telegram Bot
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-    ADMIN_TELEGRAM_ID: int = int(os.getenv("ADMIN_TELEGRAM_ID", "0"))
-    ADMIN_GROUP_ID: int = int(os.getenv("ADMIN_GROUP_ID", "0"))
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "8748909852:AAGJY_C-CE8MKxtm3VPpOPZU3liGiGKO3v4")
+    ADMIN_TELEGRAM_ID: int = int(os.getenv("ADMIN_TELEGRAM_ID", "703665167"))
+    ADMIN_GROUP_ID: int = int(os.getenv("ADMIN_GROUP_ID", "1002782538826"))
 
     # Notion
-    NOTION_TOKEN: str = os.getenv("NOTION_TOKEN", "")
+    NOTION_TOKEN: str = os.getenv("NOTION_TOKEN", "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")
 
     # Notion Databases
     DB_SERVICES: str = os.getenv("NOTION_DB_SERVICES", "")
